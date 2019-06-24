e7fa9cf65c1eb9c78accc2ee4e4b200b4d3eb036
cccen <cccen@163.com>
2019-06-24 02:53:42 +0000

    完成用户注册

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,4 +16,21 @@ class UserController extends Controller
     {
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6',
+        ]);
+
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+        session()->flash('success','欢迎开启一段奇妙的旅程！');
+        return redirect()->route('users.show',[$user]);
+    }
 }
