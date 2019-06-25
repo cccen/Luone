ff8019c10b1ba4726691c7124ef16116582ff995
cccen <cccen@163.com>
2019-06-25 09:07:03 +0000

    发布微博

--- resources/views/StaticPages/home.blade.php
+++ resources/views/StaticPages/home.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-    <div class="jumbotron">
-        <h1>撸第一个代码</h1>
-        <p class="lead">
-            撸第一个代码
-        </p>
-        <p>从这里开始</p>
-        <a href="{{route('signup')}}" class="btn btn-success btn-lg">现在注册</a>
-    </div>
+    @if(Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info',['user'=>Auth::user()])
+                </section>
+            </aside>
+        </div>
+    @else
+        <div class="jumbotron">
+            <h1>撸第一个代码</h1>
+            <p class="lead">
+                撸第一个代码
+            </p>
+            <p>从这里开始</p>
+            <a href="{{route('signup')}}" class="btn btn-success btn-lg">现在注册</a>
+        </div>
+    @endif
 @stop
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::resource('users','UserController');
 Route::get('login', 'SessionController@create')->name('login');
 Route::post('login', 'SessionController@store')->name('login');
 Route::delete('logout', 'SessionController@destroy')->name('logout');
+
+Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
