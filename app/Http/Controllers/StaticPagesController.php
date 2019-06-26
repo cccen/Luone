2b753729d36273d2e648028fc039f4fea474095c
cccen <cccen@163.com>
2019-06-26 01:36:35 +0000

    删除微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -1,13 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class StaticPagesController extends Controller
 {
-    public function home()
+    public function home(User $user)
     {
-        return view('StaticPages/home');
+        $feed_items=[];
+        if(Auth::check()){
+            $feed_items=Auth::user()->feed()->paginate(30);
+        }
+        return view('StaticPages/home',compact('feed_items','user'));
+    }
+
+    public function help()
+    {
+        return view('StaticPages.help');
+    }
+
+    public function about()
+    {
+        return view('StaticPages.about');
     }
 }
